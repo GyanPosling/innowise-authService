a7d4dd7701ed1b170cef303ec731df2229d4ede6
GyanPosling <okicok578@gmail.com>
2026-04-03 16:04:08 -0400

    tasks/KUBER-0: Fix swagger config issues: replace http/locahost on innowise.local

--- src/main/java/com/innowise/authservice/config/SwaggerConfig.java
+++ src/main/java/com/innowise/authservice/config/SwaggerConfig.java
@@ -5,24 +5,38 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${app.url:}")
+    private String appUrl;
+
     @Bean
     public OpenAPI authServiceOpenApi() {
-        return new OpenAPI()
+        OpenAPI openAPI = new OpenAPI()
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth",
                                 new SecurityScheme()
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
-                                        .bearerFormat("JWT")))
+                                .bearerFormat("JWT")))
                 .info(new Info()
                         .title("Auth Service API")
                         .version("v1"));
+
+        if (!appUrl.isBlank()) {
+            openAPI.servers(List.of(new Server()
+                    .url(appUrl)
+                    .description("Public Gateway URL")));
+        }
+
+        return openAPI;
     }
 }
